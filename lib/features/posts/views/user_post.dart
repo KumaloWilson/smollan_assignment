174937165027712abc215ca8256171b3ec930e4b
KumaloWilson <kumalowilson900@gmail.com>
2025-02-07 12:11:48 +0200

    last itereation

--- lib/features/posts/views/user_post.dart
+++ lib/features/posts/views/user_post.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-
+import 'package:readmore/readmore.dart';
+import 'package:smollan_assignment/core/constants/color_constants.dart';
 import '../../../providers/post_provider.dart';
-import '../../../widgets/post_widgets/post_actions.dart';
-import '../../../widgets/post_widgets/post_caption.dart';
-import '../../../widgets/post_widgets/post_comments.dart';
 import '../../../widgets/post_widgets/post_header.dart';
 import '../../../widgets/post_widgets/post_image.dart';
-import '../../../widgets/post_widgets/post_likes.dart';
+import '../../../widgets/post_widgets/reaction_button.dart';
 class PostScreen extends StatefulWidget {
   final String postId;
 
@@ -28,12 +27,23 @@ class _PostScreenState extends State<PostScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Post'),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        elevation: 1,
+        title: Text('Posts'),
+        backgroundColor: theme.scaffoldBackgroundColor,
+        actions: [
+          TextButton(
+            onPressed: () {
+              // Implement delete post action
+            },
+            child: Text(
+              'Follow',
+              style: TextStyle(color: Palette.instagramBlue),
+            ),
+          ),
+        ],
       ),
       body: Consumer<PostProvider>(
         builder: (context, postProvider, child) {
@@ -55,20 +65,57 @@ class _PostScreenState extends State<PostScreen> {
                     postDate: post.postDate,
                   ),
                   PostImage(imageUrl: post.image),
-                  PostActions(
-                    onLike: () => postProvider.incrementLikes(),
-                    onComment: () {/* Implement comment action */},
-                    onShare: () {/* Implement share action */},
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        ReactionButton(
+                          label: 'like',
+                          icon: FontAwesomeIcons.heart,
+                          value: '${post.likes}',
+                          onTap: ()=>  postProvider.incrementLikes(),
+                        ),
+                        ReactionButton(
+                          label: 'comment',
+                          icon: FontAwesomeIcons.comment,
+                          value: '56',
+                          onTap: () {},
+                        ),
+                        ReactionButton(
+                          label: 'share',
+                          icon: FontAwesomeIcons.paperPlane,
+                          value: '78',
+                          onTap: () {},
+                        ),
+                        Spacer(),
+                        GestureDetector(
+                            child: Icon(FontAwesomeIcons.bookmark)
+                        )
+                      ],
+                    ),
                   ),
-                  PostLikes(likes: post.likes),
-                  PostCaption(
-                    username: post.username,
-                    caption: post.caption,
-                  ),
-                  PostComments(
-                    postText: post.postText,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ReadMoreText(
+                          post.caption,
+                          trimLines: 2,
+                          colorClickableText: Colors.pink,
+                          trimMode: TrimMode.Line,
+                          trimCollapsedText: 'more',
+                          trimExpandedText: ' ',
+                          moreStyle: TextStyle(fontSize: 12,),
+                          lessStyle: TextStyle(fontSize: 12,),
 
+                        ),
+                        SizedBox(height: 4),
+                        Text(post.postDate, style: TextStyle(color: Colors.grey, fontSize: 12)),
+                      ],
+                    ),
                   ),
+
                 ],
               ),
             );
